3bf8de2e5247ba283ccea43ba18573160a660bd4
hari krishna <youremail@example.com>
2015-01-03 15:20:21 +0530

    token input

---
+++ app/models/book.rb
@@ -0,0 +1,9 @@
+class Book < ActiveRecord::Base
+	  has_many :authorships
+    has_many :authors, :through => :authorships
+    attr_reader :author_tokens
+
+    def author_tokens=(ids)
+    	self.author_ids = ids.split(",")
+    end 
+end
